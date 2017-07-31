5c1acc6085f46bb76eee743be1cf224471742fbe
Kyle Gretchev <kgretchev@gmail.com>
2017-07-31 01:26:23 -0400

    Begin replaceing owened strings with references

--- src/capabilities.rs
+++ src/capabilities.rs
@@ -2,37 +2,33 @@ use Event;
 
 #[derive(Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct CapabilitiesDescriptor {
+pub struct CapabilitiesDescriptor<'a> {
     #[serde(skip_serializing_if = "Option::is_none")]
-    api_version: Option<String>,
+    api_version: Option<&'a str>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    capabilities: Option<Capabilities>,
-    description: String,
-    key: String,
-    links: Links,
-    name: String,
+    capabilities: Option<Capabilities<'a>>,
+    description: &'a str,
+    key: &'a str,
+    links: Links<'a>,
+    name: &'a str,
     #[serde(skip_serializing_if = "Option::is_none")]
-    vendor: Option<Vendor>,
+    vendor: Option<Vendor<'a>>,
 }
 
-impl CapabilitiesDescriptor {
-    pub fn new<T, U, V>(capabilities: Capabilities,
-                        description: T,
-                        key: U,
-                        links: Links,
-                        name: V)
-                        -> Self
-        where T: AsRef<str>,
-              U: AsRef<str>,
-              V: AsRef<str>
-    {
+impl<'a> CapabilitiesDescriptor<'a> {
+    pub fn new(capabilities: Capabilities<'a>,
+               description: &'a str,
+               key: &'a str,
+               links: Links<'a>,
+               name: &'a str)
+               -> Self {
         Self {
             api_version: None,
             capabilities: Some(capabilities),
-            description: description.as_ref().into(),
-            key: key.as_ref().into(),
+            description: description,
+            key: key,
             links: links,
-            name: name.as_ref().into(),
+            name: name,
             vendor: None,
         }
     }
@@ -40,21 +36,21 @@ impl CapabilitiesDescriptor {
 
 #[derive(Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
-pub struct Capabilities {
+pub struct Capabilities<'a> {
     #[serde(skip_serializing_if = "Option::is_none")]
-    action: Option<Vec<Action>>,
+    action: Option<&'a [Action]>,
     #[serde(skip_serializing_if = "Option::is_none")]
     admin_page: Option<AdminPage>,
     #[serde(skip_serializing_if = "Option::is_none")]
     configurable: Option<Configurable>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    dialog: Option<Vec<Dialog>>,
+    dialog: Option<&'a [Dialog]>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    external_page: Option<Vec<ExternalPage>>,
+    external_page: Option<&'a [ExternalPage]>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    glance: Option<Vec<Glance>>,
+    glance: Option<&'a [Glance]>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    hipchat_api_consumer: Option<HipchatApiConsumer>,
+    hipchat_api_consumer: Option<HipchatApiConsumer<'a>>,
     #[serde(skip_serializing_if = "Option::is_none")]
     installable: Option<Installable>,
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -62,12 +58,12 @@ pub struct Capabilities {
     #[serde(skip_serializing_if = "Option::is_none")]
     oauth2_provider: Option<OAuth2Provider>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    web_panel: Option<Vec<WebPanel>>,
-    webhook: Vec<WebHook>,
+    web_panel: Option<&'a [WebPanel]>,
+    webhook: &'a [WebHook<'a>],
 }
 
-impl Capabilities {
-    pub fn new(api_consumer: HipchatApiConsumer, webhooks: Vec<WebHook>) -> Self {
+impl<'a> Capabilities<'a> {
+    pub fn new(api_consumer: HipchatApiConsumer<'a>, webhooks: &'a [WebHook<'a>]) -> Self {
         Self {
             action: None,
             admin_page: None,
@@ -86,39 +82,34 @@ impl Capabilities {
 }
 
 #[derive(Serialize, Debug)]
-pub struct Links {
+pub struct Links<'a> {
     #[serde(skip_serializing_if = "Option::is_none")]
-    homepage: Option<String>,
+    homepage: Option<&'a str>,
 
     #[serde(rename = "self")]
-    link_self: String,
+    link_self: &'a str,
 }
 
-impl Links {
-    pub fn new<T>(link_self: T) -> Self
-        where T: AsRef<str>
-    {
+impl<'a> Links<'a> {
+    pub fn new(link_self: &'a str) -> Self {
         Self {
             homepage: None,
-            link_self: link_self.as_ref().into(),
+            link_self: link_self,
         }
     }
 
-    pub fn with_homepage<T, U>(link_self: T, homepage: U) -> Self
-        where T: AsRef<str>,
-              U: AsRef<str>
-    {
+    pub fn with_homepage(link_self: &'a str, homepage: &'a str) -> Self {
         Self {
-            homepage: Some(homepage.as_ref().into()),
-            link_self: link_self.as_ref().into(),
+            homepage: Some(homepage),
+            link_self: link_self,
         }
     }
 }
 
 #[derive(Serialize, Debug)]
-struct Vendor {
-    name: String,
-    url: String,
+struct Vendor<'a> {
+    name: &'a str,
+    url: &'a str,
 }
 
 #[derive(Serialize, Debug)]
@@ -140,60 +131,51 @@ struct ExternalPage {}
 struct Glance {}
 
 #[derive(Serialize, Debug)]
-pub struct HipchatApiConsumer {
+pub struct HipchatApiConsumer<'a> {
     #[serde(skip_serializing_if = "Option::is_none")]
-    avatar: Option<Avatar>,
-    from_name: String,
-    scopes: Vec<Scope>,
+    avatar: Option<Avatar<'a>>,
+    from_name: &'a str,
+    scopes: &'a [Scope],
 }
 
-impl HipchatApiConsumer {
-    pub fn new<T>(from_name: T, scopes: Vec<Scope>) -> Self
-        where T: AsRef<str>
-    {
+impl<'a> HipchatApiConsumer<'a> {
+    pub fn new(from_name: &'a str, scopes: &'a [Scope]) -> Self {
         Self {
             avatar: None,
-            from_name: from_name.as_ref().into(),
+            from_name: from_name,
             scopes: scopes,
         }
     }
 
-    pub fn with_avatar<T>(avatar: Avatar, from_name: T, scopes: Vec<Scope>) -> Self
-        where T: AsRef<str>
-    {
+    pub fn with_avatar(avatar: Avatar<'a>, from_name: &'a str, scopes: &'a [Scope]) -> Self {
         Self {
             avatar: Some(avatar),
-            from_name: from_name.as_ref().into(),
+            from_name: from_name,
             scopes: scopes,
         }
     }
 }
 
 #[derive(Serialize, Debug)]
-pub struct Avatar {
-    url: String,
+pub struct Avatar<'a> {
+    url: &'a str,
 
     #[serde(rename = "url@2x", skip_serializing_if = "Option::is_none")]
-    url2x: Option<String>,
+    url2x: Option<&'a str>,
 }
 
-impl Avatar {
-    pub fn new<T>(url: T) -> Self
-        where T: AsRef<str>
-    {
+impl<'a> Avatar<'a> {
+    pub fn new(url: &'a str) -> Self {
         Self {
-            url: url.as_ref().into(),
+            url: url,
             url2x: None,
         }
     }
 
-    pub fn with_2x<T, U>(url: T, url2x: U) -> Self
-        where T: AsRef<str>,
-              U: AsRef<str>
-    {
+    pub fn with_2x(url: &'a str, url2x: &'a str) -> Self {
         Self {
-            url: url.as_ref().into(),
-            url2x: Some(url2x.as_ref().into()),
+            url: url,
+            url2x: Some(url2x),
         }
     }
 }
@@ -211,32 +193,28 @@ struct OAuth2Provider {}
 struct WebPanel {}
 
 #[derive(Serialize, Debug)]
-pub struct WebHook {
-    name: String,
-    url: String,
+pub struct WebHook<'a> {
+    name: &'a str,
+    url: &'a str,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pattern: Option<String>,
+    pattern: Option<&'a str>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    authentication: Option<String>,
+    authentication: Option<&'a str>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    key: Option<String>,
+    key: Option<&'a str>,
     event: Event,
 }
 
-impl WebHook {
-    pub fn new<T, U, V>(name: T, url: U, event: CapabilitiesEvent<V>) -> Self
-        where T: AsRef<str>,
-              U: AsRef<str>,
-              V: AsRef<str>
-    {
+impl<'a> WebHook<'a> {
+    pub fn new(name: &'a str, url: &'a str, event: CapabilitiesEvent<&'a str>) -> Self {
         let (event, pattern) = match event {
-            CapabilitiesEvent::RoomMessage(p) => (Event::RoomMessage, Some(p.as_ref().into())),
+            CapabilitiesEvent::RoomMessage(p) => (Event::RoomMessage, Some(p)),
             e @ _ => (Event::from(&e), None),
         };
 
         Self {
-            name: name.as_ref().into(),
-            url: url.as_ref().into(),
+            name: name,
+            url: url,
             pattern: pattern,
             authentication: None,
             key: None,
