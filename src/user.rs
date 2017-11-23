1e958c876a16d3f489e4fb25fa18c33b9f0a5598
Kyle Gretchev <kyle.gretchev@magnetforensics.com>
2017-11-22 20:19:57 -0500

    User stuff

--- src/user.rs
+++ src/user.rs
@@ -1,8 +1,23 @@
 use std::borrow::Cow;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize)]
+#[serde(untagged)]
 pub enum UserRequest<'a> {
     Add {
-        name: Cow<'a, str>
-    }
+        name: Cow<'a, str>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        roles: Option<Vec<String>>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        title: Option<Cow<'a, str>>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        mention_name: Option<Cow<'a, str>>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        is_group_admin: Option<bool>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        timezone: Option<Cow<'a, str>>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        password: Option<Cow<'a, str>>,
+        email: Cow<'a, str>,
+    },
+    Delete { id_or_email: Cow<'a, str> },
 }
